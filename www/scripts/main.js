594e7bdcab2761e81db6a4e2b37097545e40071e
beginor <beginor@qq.com>
2014-11-08 18:16:07 +0800

    project layout refact

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 requirejs.config({
-    baseUrl: 'javascripts',
+    baseUrl: 'scripts',
     paths: {
         'jquery': 'libs/jquery/2.1.1/jquery-2.1.1',
         'bootstrap': 'libs/bootstrap/3.2.0/js/bootstrap',
