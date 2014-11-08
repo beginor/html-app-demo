75bd06164ebc1785ae8843a99b932903036af815
beginor <beginor@qq.com>
2014-11-08 18:17:20 +0800

    html5 location mode

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -17,7 +17,7 @@
             app.factory = $provide.factory;
             app.service = $provide.service;
 
-            //$locationProvider.html5Mode(true);
+            $locationProvider.html5Mode(true);
 
             if (config.routes != undefined) {
                 angular.forEach(config.routes, function(route, path) {
