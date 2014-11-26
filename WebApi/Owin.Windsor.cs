b948fa6ab41e6c06193aee90e9c2a302f9237420
beginor <beginor@qq.com>
2014-11-26 07:23:52 -0800

    try windsor extensions

--- WebApi/Owin.Windsor.cs
+++ WebApi/Owin.Windsor.cs
@@ -1,17 +1,59 @@
-﻿using System.Web.Http;
-using Microsoft.Owin.Logging;
+﻿using System;
+using System.Web.Http;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
 
 namespace Owin {
 
     public static class WindsorExtensions {
 
-        public static IAppBuilder UseWindsor(this IAppBuilder appBuilder, string configPath) {
-            //appBuilder.Properties[configPath]
+        private static readonly string AppWindsorContainer = "app.windsor_container";
+
+        public static IAppBuilder UseWindsorContainer(this IAppBuilder appBuilder, string configPath) {
+            if (appBuilder == null) {
+                throw new ArgumentNullException("appBuilder");
+            }
+            if (string.IsNullOrEmpty(configPath)) {
+                throw new ArgumentNullException("configPath");
+            }
+
+            var container = new WindsorContainer();
+            var installer = Configuration.FromXmlFile(configPath);
+            container.Install(installer);
+
+            UseWindsorContainer(appBuilder, container);
             return appBuilder;
+        }
+
+        public static IAppBuilder UseWindsorContainer(this IAppBuilder appBuilder, IWindsorContainer container) {
+            if (appBuilder == null) {
+                throw new ArgumentNullException("appBuilder");
+            }
+            if (container == null) {
+                throw new ArgumentNullException("container");
+            }
+
+            var rootContainer = GetRootContainer(appBuilder);
+            rootContainer.AddChildContainer(container);
+
+            return appBuilder;
+        }
+
+        private static IWindsorContainer GetRootContainer(IAppBuilder app) {
+            if (!app.Properties.ContainsKey(AppWindsorContainer)) {
+                lock(typeof(WindsorExtensions)) {
+                    if (!app.Properties.ContainsKey(AppWindsorContainer)) {
+                        var container = new WindsorContainer();
+                        app.Properties[AppWindsorContainer] = container;
+                    }
+                }
+            }
+            return (IWindsorContainer)app.Properties[AppWindsorContainer];
         }
 
         public static IAppBuilder UseWebApiWithWindsor(this IAppBuilder appBuilder, HttpConfiguration config) {
             return appBuilder;
         }
+
     }
 }
