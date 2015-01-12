872ce62bbfe86aff93f2e9b8e016d431a01364ec
beginor <beginor@hotmail.com.com>
2015-01-11 19:58:59 -0800

    identity

---
+++ WebApi/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Http;
+using System.Web.Http.Results;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using WebApi.Models;
+
+namespace WebApi.Controllers {
+
+    [Authorize]
+    [RoutePrefix("api/account")]
+    public class AccountController : ApiController {
+
+        private ApplicationUserManager userManager;
+        private ApplicationSignInManager signInManager;
+
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager) {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("login")]
+        public IHttpActionResult Login([FromBody]LoginModel model) {
+            if (!ModelState.IsValid) {
+                return new InvalidModelStateResult(ModelState, this);
+            }
+        }
+    }
+}
---
+++ WebApi/Models/AccountModels.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models {
+
+    public class LoginModel {
+
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
+    }
+
+}
