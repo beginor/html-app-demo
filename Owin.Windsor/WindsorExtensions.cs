582c638ac374a9642f3d058fb87ff809a6b751e4
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2015-01-13 15:06:40 +0800

    owin app container bug fix.

--- Owin.Windsor/WindsorExtensions.cs
+++ Owin.Windsor/WindsorExtensions.cs
@@ -8,9 +8,9 @@ namespace Owin.Windsor {
 
         private static readonly string AppWindsorContainer = "app.windsor_container";
 
-        public static IAppBuilder UseWindsorContainer(this IAppBuilder appBuilder, string configPath) {
-            if (appBuilder == null) {
-                throw new ArgumentNullException("appBuilder");
+        public static IAppBuilder UseWindsorContainer(this IAppBuilder app, string configPath) {
+            if (app == null) {
+                throw new ArgumentNullException("app");
             }
             if (string.IsNullOrEmpty(configPath)) {
                 throw new ArgumentNullException("configPath");
@@ -20,32 +20,26 @@ namespace Owin.Windsor {
             var installer = Configuration.FromXmlFile(configPath);
             container.Install(installer);
 
-            UseWindsorContainer(appBuilder, container);
-            return appBuilder;
+            UseWindsorContainer(app, container);
+            return app;
         }
 
-        public static IAppBuilder UseWindsorContainer(this IAppBuilder appBuilder, IWindsorContainer container) {
-            if (appBuilder == null) {
-                throw new ArgumentNullException("appBuilder");
+        public static IAppBuilder UseWindsorContainer(this IAppBuilder app, IWindsorContainer container) {
+            if (app == null) {
+                throw new ArgumentNullException("app");
             }
             if (container == null) {
                 throw new ArgumentNullException("container");
             }
 
-            var rootContainer = GetWindsorContainer(appBuilder);
-            rootContainer.AddChildContainer(container);
+            app.Properties[AppWindsorContainer] = container;
 
-            return appBuilder;
+            return app;
         }
 
         public static IWindsorContainer GetWindsorContainer(this IAppBuilder app) {
             if (!app.Properties.ContainsKey(AppWindsorContainer)) {
-                lock(typeof(WindsorExtensions)) {
-                    if (!app.Properties.ContainsKey(AppWindsorContainer)) {
-                        var container = new WindsorContainer();
-                        app.Properties[AppWindsorContainer] = container;
-                    }
-                }
+                throw new InvalidOperationException("No windsor container configured.");
             }
             return (IWindsorContainer)app.Properties[AppWindsorContainer];
         }
