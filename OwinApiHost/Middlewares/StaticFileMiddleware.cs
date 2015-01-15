2f749064cff85c294fac229545ff5616d25d9cd7
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2015-01-15 14:29:28 +0800

    etag support

--- OwinApiHost/Middlewares/StaticFileMiddleware.cs
+++ OwinApiHost/Middlewares/StaticFileMiddleware.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace OwinApiHost.Middlewares {
 
@@ -25,14 +26,33 @@ namespace OwinApiHost.Middlewares {
             if (requestPath.EndsWith("/")) {
                 requestPath += options.DefaultFile;
             }
+
             var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, options.RootDirectory, requestPath.Substring(1));
+            var fileInfo = new FileInfo(filePath);
+            var ticks = fileInfo.LastWriteTimeUtc.Ticks;
+            //
+            var requestHeaders = (IDictionary<string, string[]>)env["owin.RequestHeaders"];
+            if (requestHeaders.ContainsKey("If-None-Match")) {
+                var tagValue = requestHeaders["If-None-Match"];
+                if (tagValue != null && tagValue.Length > 0) {
+                    long tag;
+                    if (long.TryParse(tagValue[0], out tag)) {
+                        if (ticks == tag) {
+                            env["owin.ResponseStatusCode"] = (int)HttpStatusCode.NotModified;
+                            env["owin.ResponseReasonPhrase"] = "Not Modified";
+                            return Task.FromResult(0);
+                        }
+                    }
+                }
+            }
             if (File.Exists(filePath)) {
                 var responseBody = (Stream)env["owin.ResponseBody"];
-                var fileInfo = new FileInfo(filePath);
+
                 var buff = File.ReadAllBytes(filePath);
                 var headers = (IDictionary<string, string[]>)env["owin.ResponseHeaders"];
                 headers["Content-Type"] = new[] { options.GetMimeType(fileInfo.Extension) };
                 headers["Content-Length"] = new[] { buff.Length.ToString() };
+                headers["ETag"] = new [] { ticks.ToString() };
                 return responseBody.WriteAsync(buff, 0, buff.Length);
             }
             return next.Invoke(env);
@@ -42,11 +62,11 @@ namespace OwinApiHost.Middlewares {
 
     public class StaticFileMiddlewareOptions {
 
-        public string RootDirectory { get; }
+        public string RootDirectory { get; private set; }
 
-        public string DefaultFile { get; }
+        public string DefaultFile { get; private set; }
 
-        public string DefaultMimeType { get; }
+        public string DefaultMimeType { get; private set; }
 
         public StaticFileMiddlewareOptions(string rootDirectory, string defaultFile, string defaultMimeType) {
             RootDirectory = rootDirectory;
