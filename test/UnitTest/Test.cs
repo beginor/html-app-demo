ad80a94bc266169e5923e9bc1e19349c1f189a6c
beginor <beginor@qq.com>
2015-12-21 15:48:37 +0800

    add unit test project.

---
+++ test/UnitTest/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UnitTest {
+
+    [TestFixture()]
+    public class Test {
+        [Test()]
+        public void TestCase() {
+        }
+    }
+}
+
