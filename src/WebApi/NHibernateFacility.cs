85fd7906da55ec25564e6534aba2ff32cc2f75b1
beginor <beginor@qq.com>
2015-12-23 19:05:04 +0800

    simple nhibernat facility

---
+++ src/WebApi/NHibernateFacility.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Castle.MicroKernel.Facilities;
+using NHibernate.Cfg;
+using Castle.MicroKernel.Registration;
+using NHibernate;
+using System.IO;
+
+namespace WebApi {
+
+    public class NHibernateFacility : AbstractFacility {
+        
+        protected override void Init() {
+            var configFile = FacilityConfig.Attributes["configFile"];
+            if (string.IsNullOrEmpty(configFile)) {
+                throw new FacilityException("configFile is null or empty!");
+            }
+            else {
+                RegisterSessionFactory(configFile);
+                RegisterSession();
+            }
+        }
+
+        private void RegisterSessionFactory(string configFile) {
+            if (!File.Exists(configFile)) {
+                throw new FacilityException("Config file not exists.");
+            }
+            Kernel.Register(
+                Component.For<ISessionFactory>()
+                .UsingFactoryMethod(() => {
+                    var cfg = new Configuration();
+                    cfg.Configure(configFile);
+                    return cfg.BuildSessionFactory();
+                }).LifestyleSingleton()
+            );
+        }
+
+        private void RegisterSession() {
+            Kernel.Register(
+                Component.For<ISession>().UsingFactoryMethod((kernel, context) => {
+                    var sessionFactory = kernel.Resolve<ISessionFactory>();
+                    return sessionFactory.OpenSession();
+                }).LifestyleTransient()
+            );
+        }
+    }
+}
+
--- test/UnitTest/NhibernateTest.cs
+++ test/UnitTest/NhibernateTest.cs
@@ -1,15 +1,44 @@
 ﻿using NUnit.Framework;
 using System;
+using NHibernate;
+using NHibernate.Cfg;
+using WebApi.Data;
+using NHibernate.Linq;
+using System.Linq;
+using Microsoft.Owin.Security.DataProtection;
+using Microsoft.AspNet.Identity;
 
 namespace UnitTest {
 
-    [TestFixture()]
-    public class NhibernateTest {
+    [TestFixture]
+    public class NhibernateTest : WindsorTest {
         
-        [Test()]
-        public void TestCase() {
-            Console.WriteLine("OK");
+        [Test]
+        public void CanBuildSessionFactory() {
+            var cfg = new Configuration();
+            cfg.Configure("hibernate.config");
+            using (var sessionFactory = cfg.BuildSessionFactory()) {
+                var session = sessionFactory.OpenSession();
+
+                var users = session.Query<ApplicationUser>().ToList();
+
+                Console.WriteLine(users.Count);
+            }
         }
+
+        [Test]
+        public void CanResolveDataProtectionProvider() {
+            var dpp = Container.Resolve<IDataProtectionProvider>();
+            Assert.IsNotNull(dpp);
+            var helloWorld = "Hello, world!";
+            var dp = dpp.Create(helloWorld);
+            var encryptd = dp.Protect(System.Text.Encoding.UTF8.GetBytes(helloWorld));
+            var clear = dp.Unprotect(encryptd);
+            var result = System.Text.Encoding.UTF8.GetString(clear);
+
+            Assert.AreEqual(helloWorld, result);
+        }
+
     }
 }
 
